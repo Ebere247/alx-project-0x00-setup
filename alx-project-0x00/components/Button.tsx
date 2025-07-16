973cfe9d7f3b6e4c45679924fdca2c0376d1afa9
Ebere Onuobia <cynthiaonuobia@gmail.com>
2025-07-16 01:46:20 +0100

    Advanced task

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -4,7 +4,7 @@ import { ButtonProps } from "../interfaces";
 
 const Button: React.FC<ButtonProps> = ({ title, styleClass }) => {
   return (
-    <button className={`bg-blue-600 text-white px-4 py-2 ${styleClass}`}>
+    <button className={`px-4 py-2 bg-blue-500 text-white ${styleClass}`}>
       {title}
     </button>
   );
